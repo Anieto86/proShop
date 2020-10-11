5bd11cbc55609a1578adc90d263eb4f186a29083
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-10-11 19:14:20 +1030

    models

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,12 +1,75 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema;
+//todo Model reviews schema | is also exported with user.
+const reviewsSchema = mongoose.Schema;
+const reviewsSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    coment: { type: String, required: true },
+  },
+  {
+    timestamp: true,
+  }
+);
+
 
 //todo Model product schema
-
-const productSchema = new Schema({
-    
-});
+const productSchema = mongoose.Schema;
+const productSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewsSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
