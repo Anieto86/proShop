f52690df4d5d40978502c7b0c23593da38c5c4f3
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-10-11 21:14:00 +1030

    seeder, no connect with mongodb

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -24,12 +24,7 @@ const orderSchema = new Schema(
         },
       },
     ],
-    shippingAddress: {
-      address: { type: String, required: true },
-      city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
-    },
+     
     paymentMethod: {
       type: String,
       required: true,
