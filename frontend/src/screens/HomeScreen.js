e9ada02cbd33bb09ebd7a00ebd77530b9c147afd
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-10-26 12:52:34 +1030

    redux state in homeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
 //todo importa reactBoostrap
 import { Row, Col } from "react-bootstrap";
 
-//Todo Axios
-import axios from "axios";
+//todo import ListProducts
+import { listProducts } from "../action/productActions";
+
+//Todo Bring dispatch REDUX
+import { useDispatch, useSelector } from "react-redux";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  //todo Dispatch se usa para llamar la accion
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   //!Request to my DB | this is like componentDidMount()
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get("/api/products");
-
-      setProducts(data);
-    }
-
-    fetchProducts();
-  },[]);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Lasterst Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
